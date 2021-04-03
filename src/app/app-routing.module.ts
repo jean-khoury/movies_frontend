efa1ec76d3057e1028df1ed10f5a111f40861f37
Jean Khoury <jeankhoury_98@hotmail.com>
2021-04-03 13:04:19 +0200

    Updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MovieListComponent } from './movie-list/movie-list.component';
-
+import { MovieFormComponent } from './movie-form/movie-form.component';
 const routes: Routes = [
-  { path: 'movies', component: MovieListComponent }
+  { path: 'movies', component: MovieListComponent },
+  { path: 'addmovie', component: MovieFormComponent }
 ];
 
 @NgModule({
