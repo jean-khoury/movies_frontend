a485348caa144168a436684f93d281a133bdd5db
Jean Khoury <jeankhoury_98@hotmail.com>
2021-04-03 12:11:43 +0200

    Added routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+
+const routes: Routes = [
+  { path: 'movies', component: MovieListComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
