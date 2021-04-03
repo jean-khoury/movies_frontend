69242ce4e9a6385691959f3d17877e3043a9a838
Jean Khoury <jeankhoury_98@hotmail.com>
2021-04-03 11:52:16 +0200

    Added movie service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/service/movie-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Movie } from '../model/movie';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class MovieServiceService {
+
+  private moviesUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.moviesUrl = 'http://localhost:4500/movies';
+  }
+  public findAll(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.moviesUrl);
+  }
+
+}
